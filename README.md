0c40e3d14b6b07a7a2058ab5b2f120c1fb618953
Paul Schneider <paulschneider@free.fr>
2017-07-04 00:54:41 +0200

    uses a secret json

---
+++ README.md
@@ -0,0 +1,5 @@
+# gapi-net451
+
+Makes Google Api more reliable,
+and simple to integrate, as a monolitic packet.
+
---
+++ test/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Google.Apis.Auth.OAuth2;
+using System.Threading.Tasks;
+using Google.Apis.Services;
+using Newtonsoft.Json;
+
+namespace test
+{
+	class MainClass
+	{
+
+		public static async Task TestOne()
+		{
+			GoogleCredential credential = await GoogleCredential.GetApplicationDefaultAsync();
+			var basClientService = new BaseClientService.Initializer()
+				{
+					HttpClientInitializer = credential
+				};
+			Console.WriteLine (JsonConvert.SerializeObject (basClientService));
+		}
+		public  static void Main (string[] args)
+		{
+			Environment.SetEnvironmentVariable ("GOOGLE_APPLICATION_CREDENTIALS", "google-secret.json");
+
+			Task.Run (async () => await TestOne ()).Wait();
+			Console.WriteLine ("Hello World!");
+
+		}
+	}
+}
